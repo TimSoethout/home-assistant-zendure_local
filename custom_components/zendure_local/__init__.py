ce57701aa2fabd26598f0b7d48c8ec9ed2a2c4bd
Tim Soethout <git@timsoethout.nl>
2025-07-03 14:26:10 +0200

    empty config schema

--- custom_components/zendure_local/__init__.py
+++ custom_components/zendure_local/__init__.py
@@ -3,18 +3,19 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+import voluptuous as vol
+from homeassistant.helpers import config_validation as cv
 
+CONFIG_SCHEMA = cv.empty_config_schema("zendure_local")
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Zendure Local integration."""
     return True
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Zendure Local from a config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
